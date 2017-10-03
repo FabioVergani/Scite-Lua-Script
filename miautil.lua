0edfcecf8559fcadca4631102e8980add88ba2f8
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-10-03 14:20:48 +0200

    Create miautil.lua

---
+++ miautil.lua
@@ -0,0 +1,20 @@
+find.replace.regexp.posix=1
+
+ext.lua.startup.script=$(SciteDefaultHome)/fabio.lua
+command.name.1.*=FABIO
+command.subsystem.1.*=3
+command.1.*=pippo
+command.mode.1.*=savebefore:no
+command.shortcut.1.*=Ctrl+U
+
+
+function pippo()
+ local s=editor:GetText()
+ s=s:gsub('(%s*[\(]%s*)','(')
+ s=s:gsub('(%s[\(][\!]%s)','(!')
+ s=s:gsub('(%s*[\)])%s*',')')
+ s=s:gsub('(%s*,%s*)',',')
+ s=s:gsub('(%s=%s)','=')
+ s=s:gsub('(%s=>%s)','=>')
+ editor:SetText(s) 
+end
